9329ca76df6684cb6ec89ec05a7931d8e208c473
Chris <147499775+chris-mathew@users.noreply.github.com>
2024-09-06 20:49:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
-import backend
+#import backend
 
 
 pickle_fil = "73.4.pkl"  # Path to the pickle file
 
-output = backend.run(pickle_fil)
+#output = backend.run(pickle_fil)
 
 
-st.title(output.item())
+#st.title(output.item())
+st.title("hello"
