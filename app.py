8044b5761aeeff3694b37e03a4c45f1f2bd52ee7
cmm223icacuk <cmm223@ic.ac.uk>
2024-09-10 21:15:56 +0100

    - typo

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ from scipy.sparse.linalg import eigsh
 
 
 def diffusion_map(correlation_matrix, n_components=3, sigma=1.2):
-    # Construct the affinity matrix directly from the correlation matrix
+
     affinity_matrix = np.exp(-pairwise_distances(correlation_matrix, metric='euclidean') ** 2 / (2. * sigma ** 2))
 
-    # Normalize the affinity matrix
+
     degree_matrix = np.diag(np.sum(affinity_matrix, axis=1))
     diffusion_operator = np.dot(np.linalg.inv(degree_matrix), affinity_matrix)
 
-    # Compute the top n_components eigenvalues and eigenvectors
+
     eigenvalues, eigenvectors = eigsh(diffusion_operator, k=n_components + 1, which='LM')
 
 
@@ -28,38 +28,38 @@ st.title('Diffusion Map Enchanced BC-GCN')
 tab1, tab2 = st.tabs(["Prediction Model", "Neural Map"])
 
 with tab1:
-    st.write("This uses the diffusion map enabled BC-GCN model to estimate and the base BC-GCN model to classify Alzheimer's Disease. Please note that the classification model isn't stable and the input must be a 112x112 corrolation matrix due to the preprocessing requiring external programs. **It is recommended that the example file be used.**")
-    # Load the saved model
+    st.write("This uses the diffusion map enabled BC-GCN model to estimate brain age and classify Alzheimer's Disease. Please note that the classification model isn't stable and the input must be a 112x112 corrolation matrix due to the preprocessing requiring external programs. **It is recommended that the example file be used.**")
 
-    pickle_file = "75.9.pkl"  # Path to the pickle file
+
+    pickle_file = "75.9.pkl"  #  pickle file
 
     uploaded_file = st.file_uploader("Choose a file", type=["pkl"])
     checkbox_state = st.checkbox("Use Example File (Age: 75.9, State: AD, Subject ID (ADNI Dataset): 018_S_4733)")
 
-    # Check the state of the checkbox
+
     if checkbox_state:
         uploaded_file = pickle_file
-    # Process the file based on its type
+
     if uploaded_file is not None:
         df = pd.read_pickle(uploaded_file)
         data_np_old = df.to_numpy()
         data_np = diffusion_map(data_np_old)
 
-        input_data = np.expand_dims(data_np, axis=0)  # Add batch dimension (shape becomes [1, height, width])
-        input_data = np.expand_dims(input_data, axis=0)  # Add channel dimension (shape becomes [1, 1, height, width])
+        input_data = np.expand_dims(data_np, axis=0)  #  [1, height, width]
+        input_data = np.expand_dims(input_data, axis=0)  # [1, 1, height, width]
         input_data = input_data.astype(np.float32)
 
-        input_data_acc = np.expand_dims(data_np_old, axis=0)  # Add batch dimension (shape becomes [1, height, width])
-        #input_data_acc = np.expand_dims(input_data_acc,axis=0)  # Add channel dimension (shape becomes [1, 1, height, width])
+        input_data_acc = np.expand_dims(data_np_old, axis=0)  # (shape becomes [1, height, width])
+
         input_data_acc = input_data_acc.astype(np.float32)
 
-        # Load the ONNX model
+        #ONNX model
         ort_session = ort.InferenceSession("model_dynamic_batch.onnx")
         input_feed = {'input.1': input_data}
         outputs = ort_session.run(None, input_feed)
         outputs = round(float(outputs[0][0][0]), 2)
 
-        # Load the ONNX model acc
+        # ONNX model acc
         ort_session_acc = ort.InferenceSession("model_new_accuracy.onnx")
         input_feed_acc = {'input.1': input_data_acc}
         outputs_acc = ort_session_acc.run(None, input_feed_acc)
@@ -83,5 +83,5 @@ with tab2:
     with open(html_file, 'r') as file:
         html_content = file.read()
 
-    # Display the HTML content in Streamlit
+
     st.components.v1.html(html_content, height=600)
